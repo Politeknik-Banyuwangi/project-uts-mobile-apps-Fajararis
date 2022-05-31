2a16133a3541d72b44d420dee26a46b72bf34a4e
FlutLab.io <>
2022-05-31 07:10:11 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -4,20 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Calls extends StatelessWidget {
   const Calls({Key? key}) : super(key: key);
-  Widget getCalls(title, subTitle) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage("https://i.postimg.cc/YCH7Q2xS/02.jpg"),
-        radius: 30,
-      ),
-      title: Text(
-        title,
-        style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
-      ),
-      subtitle: Text(subTitle),
-    );
-  } // Masukkan coding disini
-
+  // Masukkan coding disini
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,37 +15,20 @@ class Calls extends StatelessWidget {
         },
         child: ListView(
           children: [
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
-            getCalls("Ayang", "(7) Hari ini 05.25"),
             SizedBox(
               height: 10,
             ),
-
-            // Masukkan Coding disini jangan rubah lainnya
-            //
-            //
-            //
-            //
-            //
-            //
+            callPhone("Ayang", Icons.call_received, Colors.green, "Mei 20, 12:08", NetworkImage("https://i.ibb.co/BwqbJxg/ayang.jpg")),
+            callPhone("Cynthia", Icons.call_received, Colors.green, "Mei 20, 20:45", NetworkImage("https://i.ibb.co/QfpJXCq/pp2.jpg")),
+            callPhone("Fuput", Icons.call_received, Colors.green, "Mei 25, 06:30", NetworkImage("https://i.ibb.co/MSqdrmV/pp.jpg")),
+            callPhone("Clara", Icons.call_made, Colors.green, "Mei 25, 08:25", NetworkImage("https://cdn.idntimes.com/content-images/post/20180920/4faca301c0d25bbfa08bb1256a1c71e2.jpg")),
+            callPhone("Sayang", Icons.call_received, Colors.red, "Mei 25, 12:03", NetworkImage("https://lh3.googleusercontent.com/proxy/SvpJipvePYyeCRAm7GzOhJjShKePN_r4yag05BsUWH5A4_PJyqZizSz-8oarheZiORHVfGpsiRluOv8YuF3kGNeGWLi31hv0DqXeV6JKLk0CFXIvufsHmu70sA=w1200-h630-p-k-no-nu")),
+            callPhone("Clara", Icons.call_received, Colors.green, "Mei 25, 13:55", NetworkImage("https://1.bp.blogspot.com/-PgQ2tI2fbnU/X9tIboO0k2I/AAAAAAAAQWI/RqE3Lj-k31g4lvwkFwuccPZZ2Q_LzBp0wCPcBGAsYHg/s736/9e875e099e7a0ce717fa9956c464deb0.jpg")),
+            callPhone("Ayang", Icons.call_received, Colors.red, "Mei 25, 21:15", NetworkImage("https://i.pinimg.com/736x/43/c1/66/43c166f2ef2613ad393eccdff5cac10a.jpg")),
+            callPhone("Sayang", Icons.call_received, Colors.red, "Mei 28, 07:20", NetworkImage("https://i.ibb.co/njWWvq0/pp3.jpg")),
+            callPhone("Cynthia", Icons.call_received, Colors.red, "Mei 28, 07:30", NetworkImage("https://i.ibb.co/njWWvq0/pp3.jpg")),
+            callPhone("Ibuk", Icons.call_received, Colors.red, "Mei 28, 08:00", NetworkImage("https://i.ibb.co/njWWvq0/pp3.jpg")),
+            callPhone("Adek", Icons.call_received, Colors.red, "Mei 28, 08:30", NetworkImage("https://i.ibb.co/njWWvq0/pp3.jpg")),
           ],
         ),
       ),
@@ -86,6 +56,41 @@ class Calls extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget callPhone(String name, IconData iconSitus, Color iconColor, String Time, NetworkImage image) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 0.9),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: image,
+          radius: 40,
+        ),
+        title: Text(
+          name,
+          style: TextStyle(fontWeight: FontWeight.w700),
+        ),
+        subtitle: Row(
+          children: [
+            Icon(
+              iconSitus,
+              color: iconColor,
+              size: 20,
+            ),
+            SizedBox(width: 6),
+            Text(
+              Time,
+              style: TextStyle(fontSize: 13),
+            ),
+          ],
+        ),
+        trailing: Icon(
+          Icons.call,
+          size: 28,
+          color: Colors.teal,
+        ),
       ),
     );
   }
