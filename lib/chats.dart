21f6ddd4d5a363806d513387cb19f371f101bb50
FlutLab.io <>
2022-05-31 08:01:29 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -180,7 +180,7 @@ class Chats extends StatelessWidget {
             ),
             ListTile(
               leading: CircleAvatar(
-                backgroundImage: NetworkImage("https://i.postimg.cc/YCH7Q2xS/02.jpg"),
+                backgroundImage: NetworkImage("https://i.postimg.cc/bJGqjk2w/17.jpg"),
                 radius: 30,
               ),
               title: Text(
